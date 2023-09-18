be84793a9f402f82e0922f74ccd20390ac733b39
clode <cloderaldo@gmail.com>
2023-09-18 14:41:44 +0800

    secure key

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:taghole/firebase_options.dart';
 
@@ -11,6 +12,7 @@ import 'Screens/Authentication/views/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
